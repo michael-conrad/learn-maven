76895daf6ae717ae3a2a475f284883e1e6d5c8a5
Michael Conrad <m.conrad.202@gmail.com>
2025-10-04 16:51:31 -0400

    Initialize Maven-based Spring Boot project with wrapper and basic configuration.

---
+++ src/test/java/com/michael_conrad/learn_maven/LearnMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michael_conrad.learn_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
